5b38bc0aba395b8242f221ec1da7c591e518649e
Vatareg <ilinincorp@mail.ru>
2022-04-18 16:45:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("ToTEST!!!!");
+        for(int t = 0; t<100; t++){
+            System.out.println(t);
+        }
+    }
+
+}
